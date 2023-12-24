3a66d237b128ee7d0e7a01ba377a1068bd3d7113
Mikhail <mihailsavuskin13@gmail.com>
2023-12-24 23:30:42 +0300

    routeSkill

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const routeSkill = require('../src/controller/skill.controller');
 
 app.use(bodyParser.json());
 app.use('/user', routeUser);
-app.use('/skil', routeSkill); 
+app.use('/skill', routeSkill); 
 app.use((error, req, res, _next) => res.send(error.message));
 
 module.exports = app;
