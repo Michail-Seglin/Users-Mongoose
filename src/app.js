6cbff19eb1755e1e6ff4464f6452cf9154d54ccb
Mikhail <mihailsavuskin13@gmail.com>
2024-01-25 23:06:41 +0300

    jwtBuildRes

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const routeUser = require('../src/controller/user.controller');
 const routeSkill = require('../src/controller/skill.controller');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
 
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 app.use(bodyParser.json());
+app.user(cookieParser());
 app.use('/user', routeUser);
-app.use('/skill', routeSkill); 
+app.use('/skill', routeSkill);
 app.use((error, req, res, _next) => res.send(error.message));
 
 module.exports = app;
