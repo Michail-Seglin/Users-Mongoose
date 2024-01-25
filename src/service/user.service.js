e09cda5c19918114ba65bc051d4709b72803dcc4
Mikhail <mihailsavuskin13@gmail.com>
2024-01-26 00:47:55 +0300

    createUser

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,9 +1,13 @@
 const { createUserDB, getAllUsersDB, getUserIdDB, updateUserDB, deleteUserDB } = require("../repository/user.repository");
+const createToken = require('../helper/jwt');
+const bcrypt = require('bcrypt')
 
-async function createUser(user) {
-    const data = await createUserDB(user);
-    return data;
-}
+const salt = 10;
+
+// async function createUser(user) {
+//     const data = await createUserDB(user);
+//     return data;
+// }
 
 async function getAllUsers() {
     const data = await getAllUsersDB();
@@ -15,6 +19,15 @@ async function getUserId(_id) {
     return data;
 }
 
+async function createUser(user) {
+    const findUser = await getUserIdDB(user);
+    if (findUser.length) throw new Error('is user already exist');
+    const hashPassword = await bcrypt.hash(user.passworf, salt);
+
+    const data = await createUserDB({ ...user, password: hashPassword });
+    if (!data.length) throw new Error('user is not created');
+    return data;
+}
 
 async function updateUser(_id, user) {
     const data = await updateUserDB(_id, user);
